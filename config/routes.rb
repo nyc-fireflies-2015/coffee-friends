cc2c88f5d76fab3a1893d99f9cb5718646faf370
Kelly Ripple <email": null>
2015-09-17 17:17:46 -0400

    fix welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
