965fd43316301eb4faff4af41e651c4cabc73061
Travis Allen <email": null>
2015-09-17 21:09:07 -0400

    create menu_item model tests

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -4,5 +4,6 @@ class MenuItem < ActiveRecord::Base
 
 	validates_presence_of :name, :price
 	validates :name, length: {maximum: 30}
-	## price validation
+  validates :price, :format => { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0.99}
+
 end
