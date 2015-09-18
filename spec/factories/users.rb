ff49e39d1d79f84ab5626d9ce2a825a124cf4a02
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-18 10:35:32 -0400

    add phone number format validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,8 @@ FactoryGirl.define do
 	factory :user do 
 		username {Faker::Name.first_name}
 		email {Faker::Internet.email}
-		phone Random.new.rand(1_000_000_000..9_999_999_999).to_s
+		phone Random.new.rand(1_000_000_000..9_999_999_999)
 		password "supersecure"
-
-		factory :valid_phone_user do 
-			phone "+12545677654"
-		end	
 	end	
 
 end	
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,6 @@
 require 'rails_helper'
-require 'phony_rails'
 
 RSpec.describe User, type: :model do
-
-	context 'phone number normalizes properly' do 
-		it 'formats a phone number separated by dashes' do 
-			expect(PhonyRails.normalize_number('206-544-6643', country_code: 'US')).to eq('+12065446643')
-		end	
-
-		it 'formats a phone number separated by dots and slashes' do 
-			expect(PhonyRails.normalize_number('206.544/6643', country_code: 'US')).to eq('+12065446643')
-		end	
-
-		it 'formats a phone number with no special characters' do 
-			expect(PhonyRails.normalize_number('2065446643', country_code: 'US')).to eq('+12065446643')
-		end	
-	end
 
   context 'contains valid data' do
 	  it { expect(subject).to validate_presence_of(:username) }
@@ -26,18 +11,13 @@ RSpec.describe User, type: :model do
 	  it { expect(subject).to validate_length_of(:username).is_at_most(50) }
 	 	it { expect(subject).to validate_length_of(:email).is_at_most(50) }
 		it { expect(subject).to have_secure_password }
-		# it { expect(FactoryGirl.create(:valid_phone_user)).to validate_uniqueness_of(:phone) }
+		it { expect(subject).to allow_value("7667890234").for(:phone) }
+		it { expect(subject).to_not allow_value("87659").for(:phone)  }
+		it { expect(subject).to_not allow_value("kj78djska1").for(:phone)  } 
+		it { expect(FactoryGirl.build(:user)).to validate_uniqueness_of(:email) }
+		it { expect(FactoryGirl.build(:user)).to validate_uniqueness_of(:username) }
+		it { expect(FactoryGirl.build(:user)).to validate_uniqueness_of(:phone) }
 	end
-
-	context 'uniqueness validations pass' do 
-
-		user = FactoryGirl.create(:user) 
-		user.update_attributes(phone: PhonyRails.normalize_number(user.phone, country_code: 'US'))
-		user.reload
-
-		it { expect(user).to validate_uniqueness_of(:email) }
-		it { expect(user).to validate_uniqueness_of(:username) }
-	end	
 
 	context 'has correct associations' do
 		it { expect(subject).to have_many(:given_coffees) }
