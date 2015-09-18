6824f2d3c866df527701809bb4d2695189180cc9
Travis Allen <email": null>
2015-09-18 09:03:30 -0400

    add post users and cafes sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@ Rails.application.routes.draw do
 
   root 'cafes#index'
 
-  resources :cafes, only: [:index, :show] do 
+  resources :cafes, only: [:index, :show] do
     resources :menu_items, only: [:destroy, :create, :update]
     resources :coffee_gifts, only: [:new, :create]
   end
 
   get '/login' => 'user_sessions#new'
-  delete '/logout' => 'user_sessions#destroy'  
+  post '/users/sessions' => 'user_sessions#create'
+  delete '/logout' => 'user_sessions#destroy'
   get '/profile' => 'users#show'
   get '/register' => 'users#new'
   get '/cafes/login' => 'cafe_sessions#new'
-  delete '/cafes/logout' => 'cafe_sessions#destroy'  
+  post '/cafes/sessions' => 'cafe_sessions#create'
+  delete '/cafes/logout' => 'cafe_sessions#destroy'
   get '/cafes/profile' => 'cafes#show'
 
   resources :users, only: [:create]
