621c255adb51d5936f2d39633f401cbe0631a443
Travis Allen <email": null>
2015-09-18 15:03:10 -0400

    update routes and cafes/cafes_sessions controller to have profile route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root 'cafes#index'
 
   get '/cafes/login' => 'cafe_sessions#new'
-  post '/cafes/sessions' => 'cafe_sessions#create'
-  delete '/cafes/logout' => 'cafe_sessions#destroy'
-  get '/cafes/profile/:id' => 'cafes#show'
+  get '/cafes/profile' => 'cafes#show'
 
   resources :cafes, only: [:index, :show] do
     resources :menu_items, only: [:destroy, :create, :update]
@@ -15,8 +13,12 @@ Rails.application.routes.draw do
   get '/login' => 'user_sessions#new'
   post '/users/sessions' => 'user_sessions#create'
   delete '/logout' => 'user_sessions#destroy'
-  get '/profile/:id' => 'users#show', as: 'profile'
+  get '/profile' => 'users#show'
   get '/register' => 'users#new'
+
+
+  post '/cafes/sessions' => 'cafe_sessions#create'
+  delete '/cafes/logout' => 'cafe_sessions#destroy'
 
 
   resources :users, only: [:create]
