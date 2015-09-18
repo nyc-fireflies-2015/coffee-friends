717c7ec5536d87f810030d7aa03a864be199f2b6
Travis Allen <email": null>
2015-09-18 13:06:28 -0400

    re-arrange routes to allow for cafe login

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/cafes/login' => 'cafe_sessions#new'
   post '/cafes/sessions' => 'cafe_sessions#create'
   delete '/cafes/logout' => 'cafe_sessions#destroy'
-  get '/cafes/profile' => 'cafes#show'
+  get '/cafes/profile/:id' => 'cafes#show'
 
   resources :cafes, only: [:index, :show] do
     resources :menu_items, only: [:destroy, :create, :update]
