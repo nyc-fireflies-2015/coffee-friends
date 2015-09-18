13d2e85877efa78b78d0fc98791d1e5464269f81
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-17 21:44:43 -0400

    add tests for phone num validation and normalization

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,16 @@ class User < ActiveRecord::Base
 
 	has_many :given_coffees, class_name: :CoffeeGift, foreign_key: :giver_id
 	has_many :received_coffees, class_name: :CoffeeGift, foreign_key: :receiver_id
+	
+	phony_normalize :phone, default_country_code: 'US'
+	validates_plausible_phone :phone, presence: true
 
-	validates_presence_of :username, :email, :phone
+	validates_presence_of :username, :email
 	validates_uniqueness_of :username, :email, :phone
 	validates :username, length: {maximum: 20}
 	validates :email, length: {maximum: 50}
 	validates_email_format_of :email, message: "is not in the correct format"
 	validates :password, :presence => true, :length => {minimum: 6}, :on => :create
-	##write validation for phone number
 
 	def reward
 		##what will this return?
