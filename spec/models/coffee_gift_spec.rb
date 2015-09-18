cad4f3081dfa02fc77d5d2316103ce8567c8dd1c
Kelly Ripple <email": null>
2015-09-17 20:01:17 -0400

    write shoulda matcher for coffee gifts

---
+++ spec/models/coffee_gift_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe CoffeeGift do
+
+  context 'associations' do
+
+    it 'should belong to menu items' do
+      should belong_to :menu_item
+    end
+
+    it 'should belong to receiver' do
+      should belong_to :receiver
+    end
+
+    it 'should belong to giver' do
+      should belong_to :giver
+    end
+  end
+
+  context 'delegation' do
+    it 'should delegate name to menu items' do
+      should delegate_method(:name).to(:menu_item)
+    end
+  end
+
+end
