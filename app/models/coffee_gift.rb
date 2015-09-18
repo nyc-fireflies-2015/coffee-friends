0fdc428f8b3b67f105695cb81c8e5018151ce182
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-18 13:49:56 -0400

    sanitize params before creating new gift

--- app/models/coffee_gift.rb
+++ app/models/coffee_gift.rb
@@ -1,9 +1,13 @@
 class CoffeeGift < ActiveRecord::Base
-	# include TwilioTextSender
-	
 	belongs_to :menu_item
 	belongs_to :receiver, class_name: :User
 	belongs_to :giver, class_name: :User
 
 	delegate :name, to: :menu_item
+
+	def sanitize_params(params)
+		receiver = User.find_by(username: params[:coffee_gift][:receiver])
+		menu_item = MenuItem.find_by(id: params[:coffee_gift][:menu_item])
+		coffee_gift_params.merge(receiver: receiver, menu_item: menu_item)
+	end	
 end
