9bbfda6bab51373492c95a3fef510fe0733a7b6f
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-18 17:26:30 -0400

    wip

--- spec/controllers/coffee_gifts_controller_spec.rb
+++ spec/controllers/coffee_gifts_controller_spec.rb
@@ -7,9 +7,14 @@ describe CoffeeGiftsController do
 	let(:cafe) { FactoryGirl.create(:cafe) }
 	let(:coffee_gift) { FactoryGirl.create(:coffee_gift) }
 	let(:menu_item) { FactoryGirl.create(:menu_item) }
+	let(:twilio_receiver) { FactoryGirl.create(:twilio_receiver) }
 
 	def coffee_gift_attrs
-		FactoryGirl.attributes_for(:coffee_gift).merge(receiver: user, menu_item: menu_item)
+		FactoryGirl.attributes_for(:coffee_gift).merge(receiver: twilio_receiver, menu_item: menu_item)
+	end	
+
+	def coffee_gift_post_attrs
+		FactoryGirl.attributes_for(:coffee_gift).merge(receiver: twilio_receiver.username, menu_item: menu_item)
 	end	
 
 	context '#new' do 
@@ -27,24 +32,22 @@ describe CoffeeGiftsController do
 	
 	context '#create' do 
 
-		let(:coffee_gift_attrs) { FactoryGirl.attributes_for(:coffee_gift)}
-
 		it 'redirects to root if not logged in' do 
-			post :create, cafe_id: cafe, coffee_gift: coffee_gift_attrs
+			coffee_gift_attributes = FactoryGirl.attributes_for(:coffee_gift)
+			post :create, cafe_id: cafe, coffee_gift: coffee_gift_attributes
 			expect(response).to redirect_to(root_path)
 		end
 		
 		it 'creates a new coffee gift' do 
+			log_in_user(user)
+			expect{post :create, cafe_id: cafe, coffee_gift: coffee_gift_post_attrs}.to change{CoffeeGift.all.count}.by(1)
 		end
 
-		it 'notifies the receiver of the gift' do 
-		end
-
-		it 'redirects to a confirmation page' do 
-		end
-
-		it 'does not create a coffee gift with invalid attributes' do 
-		end	
+		# it 'redirects to a confirmation page' do 
+		# 	log_in_user(user)
+		# 	post :create, cafe_id: cafe, coffee_gift: coffee_gift_post_attrs
+		# 	expect(response).to redirect_to()
+		# end
 	end
 
 	context '#show' do 
@@ -57,26 +60,24 @@ describe CoffeeGiftsController do
 		it 'redirects to root if not authorized to view coffee gift' do 
 			current_user = FactoryGirl.create(:user)
 			log_in_user(current_user)
-			coffee_gift.update_attributes(coffee_gift_attrs)
-			# binding.pry
 			get :show, id: coffee_gift
 			expect(response).to redirect_to(root_path)
 		end	
 
-		# it 'renders the show coffee gift view' do 
-		# 	log_in_user(user)
-		# 	# coffee_gift = user.received_coffees.create(FactoryGirl.attributes_for(:coffee_gift))
-		# 	get :show, id: coffee_gift
-		# 	expect(response).to render_template(:show)
-		# end
+		it 'renders the show coffee gift view if coffee belongs to current user' do 
+			log_in_user(user)
+			coffee_gift = user.received_coffees.create(coffee_gift_attrs)
+			get :show, id: coffee_gift
+			expect(response).to render_template(:show)
+		end
 		
-		# it 'locates the requested coffee gift' do 
-		# 	get :show, id: coffee_gift
-		# 	menu_item.update_attributes(cafe: cafe)
-		# 	coffee_gift.update_attributes(menu_item: menu_item)
-		# 	expect(assigns(:coffee_gift)).to eq(coffee_gift)
-		# 	expect(assigns(:cafe)).to eq(coffee_gift.cafe)
-		# end	
+		it 'locates the requested coffee gift' do 
+			log_in_user(user)
+			coffee_gift = user.received_coffees.create(coffee_gift_attrs)
+			get :show, id: coffee_gift
+			expect(assigns(:coffee_gift)).to eq(coffee_gift)
+			expect(assigns(:cafe)).to eq(coffee_gift.cafe)
+		end	
 	end	
 
 end	
