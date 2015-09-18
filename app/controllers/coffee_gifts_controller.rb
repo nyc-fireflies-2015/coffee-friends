a0547d46f5a0171d9690e90b323e8845b9cc0f49
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-18 11:33:20 -0400

    add new coffee_gift action

--- app/controllers/coffee_gifts_controller.rb
+++ app/controllers/coffee_gifts_controller.rb
@@ -1,2 +1,21 @@
 class CoffeeGiftsController < ApplicationController
+
+	def new
+		@cafe = Cafe.find_by(id: params[:cafe_id])
+		@menu_items = @cafe.menu_items
+		@coffee_gift = CoffeeGift.new
+	end	
+
+	def create
+
+	end
+
+	def show
+	end	
+	
+	private
+
+	def coffee_gift_params
+	end	
+
 end	
