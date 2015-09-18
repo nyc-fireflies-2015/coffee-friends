2d1ec2d14db675cefdead780ad251ad428070862
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-18 12:33:34 -0400

    add action for creating new coffee gift

--- app/controllers/coffee_gifts_controller.rb
+++ app/controllers/coffee_gifts_controller.rb
@@ -2,6 +2,7 @@ class CoffeeGiftsController < ApplicationController
 
 	before_action :authenticate_user
 	before_action :authorize_user, only: [:show]
+	before_action :find_cafe, except: [:new]
 
 	def new
 		@cafe = Cafe.find_by(id: params[:cafe_id])
@@ -10,16 +11,24 @@ class CoffeeGiftsController < ApplicationController
 	end	
 
 	def create
-		#authorize user
 		coffee_gift = current_user.given_coffees.build(coffee_gift_params)
-		flash[:error] = coffee_gift.errors.full_messages unless coffee_gift.save
-
+		if coffee_gift.save
+			binding.pry
+			redirect_to root_path
+		else
+			flash[:error] = coffee_gift.errors.full_messages 
+			redirect_to new_cafe_coffee_gift_path(@cafe)
+		end	
 	end
 
 	def show
 	end	
 	
 	private
+
+	def find_cafe
+		@cafe = Cafe.find_by(id: params[:cafe_id])
+	end	
 
 	def authorize_user
 		redirect_to root_path unless current_user==@coffee_gift.receiver
@@ -29,8 +38,14 @@ class CoffeeGiftsController < ApplicationController
 		redirect_to root_path unless current_user
 	end	
 
+	def coffee_gift_basic_params
+		params.require(:coffee_gift).permit(:message)
+	end	
+
 	def coffee_gift_params
-		params.require(:coffee_gift).permit(:message, :menu_item, :receiver)
+		receiver = User.find_by(username: params[:coffee_gift][:receiver])
+		menu_item = MenuItem.find_by(id: params[:coffee_gift][:menu_item])
+		coffee_gift_basic_params.merge(receiver: receiver, menu_item: menu_item)
 	end	
 
 end	
