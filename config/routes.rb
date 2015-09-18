74a9e661aa7a6d827c4f21f68f072d38653af3f6
Travis Allen <email": null>
2015-09-18 12:46:14 -0400

    re-order routes so that we can hit cafe session route, rather than cafe/:id route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root 'cafes#index'
 
+  get '/cafes/login' => 'cafe_sessions#new'
+  post '/cafes/sessions' => 'cafe_sessions#create'
+  delete '/cafes/logout' => 'cafe_sessions#destroy'
+  get '/cafes/profile' => 'cafes#show'
+
   resources :cafes, only: [:index, :show] do
     resources :menu_items, only: [:destroy, :create, :update]
     resources :coffee_gifts, only: [:new, :create]
@@ -12,10 +17,7 @@ Rails.application.routes.draw do
   delete '/logout' => 'user_sessions#destroy'
   get '/profile/:id' => 'users#show', as: 'profile'
   get '/register' => 'users#new'
-  get '/cafes/login' => 'cafe_sessions#new'
-  post '/cafes/sessions' => 'cafe_sessions#create'
-  delete '/cafes/logout' => 'cafe_sessions#destroy'
-  get '/cafes/profile' => 'cafes#show'
+
 
   resources :users, only: [:create]
   resources :coffee_gifts, only: [:show]
