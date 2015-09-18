bcbf2394c569a4134b8549919ce914f2665192af
Travis Allen <email": null>
2015-09-18 08:52:30 -0400

    create user sessions controller with new, create, and destroy routes

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,21 @@
 class UserSessionsController < ApplicationController
-end	
+  def new
+  end
+
+  def create
+    #make sure to add this route
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to root_url, notice: "Login Successful!"
+    else
+      redirect_to login_path, flash: {error: 'Username/Password combination is incorrect'}
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:notice] = "You have successfully logged out."
+    redirect_to login_path
+  end
+end
