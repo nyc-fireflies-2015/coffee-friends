5f1dee6b85d845fe087a91ecfd3b8de2cda6b0d9
Kelly Ripple <email": null>
2015-09-18 12:03:22 -0400

    debug cafe factory

--- spec/factories/cafes.rb
+++ spec/factories/cafes.rb
@@ -2,10 +2,10 @@ FactoryGirl.define do
   factory :cafe do
     name { Faker::Company.name }
     address {
-      Faker::Address.street_address +", "+
-      Faker::Address.city +", "+
-      Faker::Address.state_abbr +", "+
-      Faker::Address.zip
+      "#{Faker::Address.street_address},
+      #{Faker::Address.city},
+      #{Faker::Address.state_abbr},
+      #{Faker::Address.zip}"
     }
     password "password"
     sequence(:username) { |n| "user#{n}" }
