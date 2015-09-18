6d2788bc73d3007105017262743ad848c2fd4f9b
Travis Allen <email": null>
2015-09-18 17:20:33 -0400

    update user factories to have invalid user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,17 @@
-FactoryGirl.define do 
+FactoryGirl.define do
 
-	factory :user do 
+	factory :user do
 		username {Faker::Name.first_name}
 		email {Faker::Internet.email}
 		phone Random.new.rand(1_000_000_000..9_999_999_999)
 		password "supersecure"
-	end	
 
-end	
+
+    factory :invalid_user do
+      password "short"
+    end
+
+  end
+
+
+end
