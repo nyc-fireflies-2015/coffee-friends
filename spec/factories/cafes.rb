2a10803a3817990485c034210b4ed9877929f9c3
Kelly Ripple <email": null>
2015-09-18 11:33:41 -0400

    refactor factory

--- spec/factories/cafes.rb
+++ spec/factories/cafes.rb
@@ -3,18 +3,7 @@ FactoryGirl.define do
     name "Bluestone Lane"
     address "30 Broad St, New York, NY 10004"
     password "password"
-
-    sequence :username do |n|
-      "user#{n}"
-    end
-
-    sequence :email do |n|
-      "cafe#{n}@example.com"
-    end
-
-    factory(:invalid_cafe) do
-        username nil
-        email nil
-    end
+    sequence(:username) { |n| "user#{n}" }
+    sequence(:email) { |n| "cafe#{n}@example.com" }
   end
 end
