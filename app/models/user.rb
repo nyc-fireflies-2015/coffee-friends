223ad5ea2aa0a9ee36ea21861ed2b57945d70a5e
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-17 21:07:18 -0400

    change user-gift relationship so you can see both given and received gifts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
-	has_many :coffee_gifts, foreign_key: :giver_id
-	has_many :coffee_gifts, foreign_key: :receiver_id
+	has_many :given_coffees, class_name: :CoffeeGift, foreign_key: :giver_id
+	has_many :received_coffees, class_name: :CoffeeGift, foreign_key: :receiver_id
 
 	validates_presence_of :username, :email, :phone
+	validates_uniqueness_of :username, :email, :phone
 	validates :username, length: {maximum: 20}
 	validates :email, length: {maximum: 50}
 	validates_email_format_of :email, message: "is not in the correct format"
