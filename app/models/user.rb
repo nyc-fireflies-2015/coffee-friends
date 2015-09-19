22149e83b587bf4489a8e81e40fa9cdbf2ca56ab
Travis Allen <email": null>
2015-09-19 10:39:22 -0400

    add 10 digit phone length validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ActiveRecord::Base
 
 	has_many :given_coffees, class_name: :CoffeeGift, foreign_key: :giver_id
 	has_many :received_coffees, class_name: :CoffeeGift, foreign_key: :receiver_id
-	
+
 	before_save :normalize_phone
 
 	validates_presence_of :username, :email, :phone
 	validates_uniqueness_of :username, :email, :phone
 	validates :username, length: {maximum: 50}
+	validates_length_of :phone, :is => 10
 	validates :email, :username, length: {maximum: 50}
 	validates_email_format_of :email, message: "is not in the correct format"
 	validates_format_of :phone, with: /\d{10}/, message: "is not in the correct format"
@@ -16,15 +17,15 @@ class User < ActiveRecord::Base
 
 	def reward
 		##what will this return?
-	end	
-	
+	end
+
 	def received_coffee?(coffee_gift)
 		self == coffee_gift.receiver
-	end	
+	end
 
 	private
 	def normalize_phone
 		self.phone = "+1" + phone.to_s
-	end	
+	end
 
 end
