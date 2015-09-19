8752c3eb7d759ea15da58dd2f8ea28dbd4fc1cd6
Kelly Ripple <email": null>
2015-09-19 16:23:03 -0400

    fix display issue

--- app/controllers/cafe_sessions_controller.rb
+++ app/controllers/cafe_sessions_controller.rb
@@ -8,7 +8,7 @@ class CafeSessionsController < ApplicationController
     if @cafe && @cafe.authenticate(cafe_session_params[:password])
       log_in_cafe(@cafe)
       redirect_to cafes_profile_path
-    elsif cafe
+    elsif @cafe
       flash[:password_error] = "Incorrect password"
       render :new
     else
