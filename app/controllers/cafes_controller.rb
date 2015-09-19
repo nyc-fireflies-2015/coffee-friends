7b03b5f08f77c5b4305f65a5a0bbde09d018ff8a
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-18 20:40:34 -0400

    add submit buttons for new and edit items

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -7,6 +7,7 @@ class CafesController < ApplicationController
   def show
     @cafe = Cafe.find_by(id: params[:id])
     @cafe = current_cafe unless @cafe
+    @menu_item = MenuItem.new
     @menu_items = @cafe.menu_items
   end
 end
