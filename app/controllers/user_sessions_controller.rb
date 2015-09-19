935555e1af4ce3ed3bd59594467c465cc3c77565
Travis Allen <email": null>
2015-09-19 14:31:28 -0400

    accepts email params

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: user_session_params[:username])
+    @user = User.find_by(email: user_session_params[:email])
     if @user && @user.authenticate(user_session_params[:password])
       log_in_user(@user)
       flash[:notice] = "Login Successful!"
@@ -25,6 +25,6 @@ class UserSessionsController < ApplicationController
   private
 
   def user_session_params
-    params.require(:user_session).permit(:username, :password)
+    params.require(:user_session).permit(:email, :password)
   end
 end
