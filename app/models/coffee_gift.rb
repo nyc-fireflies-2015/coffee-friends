5a6313deb599ae2126b44720fb559b0ad3307abd
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-19 14:47:54 -0400

    add method to set phone num before save

--- app/models/coffee_gift.rb
+++ app/models/coffee_gift.rb
@@ -3,6 +3,14 @@ class CoffeeGift < ActiveRecord::Base
 	belongs_to :receiver, class_name: :User
 	belongs_to :giver, class_name: :User
 
+	before_save :set_phone_num
+
 	delegate :name, to: :menu_item
 	delegate :cafe, to: :menu_item
+
+	private
+
+	def set_phone_num
+		self.phone ||= receiver.phone
+	end	
 end
