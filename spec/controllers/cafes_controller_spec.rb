79db4c998c7cbf64868a2a27743cc3c319973a29
Kelly Ripple <email": null>
2015-09-20 17:11:09 -0400

    add tests for show route

--- spec/controllers/cafes_controller_spec.rb
+++ spec/controllers/cafes_controller_spec.rb
@@ -12,7 +12,7 @@ describe CafesController do
       end
 
       it 'assigns all cafes to @cafes' do
-        expect(@cafes).to eq(Cafe.all)
+        expect(assigns(:cafes)).to eq(Cafe.all)
       end
 
       it 'renders the correct page' do
@@ -30,6 +30,14 @@ describe CafesController do
 
       it 'assigns the correct cafe to @cafe' do
         expect(assigns(:cafe)).to eq(cafe)
+      end
+
+      it 'assigns a new menu item to @menu_item' do
+        expect(assigns(:menu_item)).to be_a(MenuItem)
+      end
+
+      it 'assigns a cafes menu items to @menu_items' do
+        expect(assigns(:menu_items)).to eq(cafe.menu_items)
       end
 
       it 'renders the correct page' do
