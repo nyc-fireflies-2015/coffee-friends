7a0bd76d712ddab1692922497bccbd143d8aa02d
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-20 17:34:03 -0400

    add user and cafe login to same modal popup

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
 	});
 
 	$("a[rel*=leanModal]").leanModal({top : 150, overlay : 0.6, closeButton: ".modal_close" });
+
+	$("#login-as-cafe").on("click", function(e){
+		e.preventDefault();
+		$("#user-login").hide();
+		$("#cafe-login").show();
+	})
+
+	$("#login-as-user").on("click", function(e){
+		e.preventDefault();
+		$("#cafe-login").hide();
+		$("#user-login").show();
+	})
 })
