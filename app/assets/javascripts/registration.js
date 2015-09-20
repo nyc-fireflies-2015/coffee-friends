fd145169dc1c7415232532f4f7476a0d6a7dbafa
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-20 11:41:45 -0400

    add modal pop-ups for both login and reg

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-	$("#coffee_gift_receiver").chosen({placeholder_text_single: "Search for a friend"});
-	$("#coffee_gift_menu_item").chosen({placeholder_text_single: "Choose menu item"});
-	$("a[rel*=register-modal-trigger]").leanModal({top : 150, overlay : 0.6, closeButton: ".modal_close" });
-	$("#login-modal-trigger").leanModal({top : 150, overlay : 0.6, closeButton: ".modal_close" });
+
+	$("#coffee_gift_receiver").chosen({
+		placeholder_text_single: "Search for a friend",
+		no_results_text: "There's no registered coffee friend named"
+	});
+
+	$("#coffee_gift_menu_item").chosen({
+		placeholder_text_single: "Choose menu item",
+		disable_search_threshold: 5
+	});
+
+	$("a[rel*=leanModal]").leanModal({top : 150, overlay : 0.6, closeButton: ".modal_close" });
+	// $("#login-modal-trigger").leanModal({top : 150, overlay : 0.6, closeButton: ".modal_close" });
 
 	// $(function () {
 	//     // Calling Login Form
