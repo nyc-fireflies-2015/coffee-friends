67d8d77f00e0b6456bbe04bfc27b4623e876f833
Kelly Ripple <email": null>
2015-09-20 11:56:40 -0400

    debug cafe controller

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,30 +1,26 @@
 class CafesController < ApplicationController
-  before_action :find_cafe, {except: :index}
 
   def index
     @cafes = Cafe.all
   end
 
   def show
+    @cafe = Cafe.find_by(id: params[:id])
     @cafe = current_cafe unless @cafe
     @menu_item = MenuItem.new
     @menu_items = @cafe.menu_items
   end
 
   def borough
+    @cafe = Cafe.find_by(id: params[:cafe_id])
     @cafes = @cafe.filter_by_borough
     render :index
   end
 
   def neighborhood
+    @cafe = Cafe.find_by(id: params[:cafe_id])
     @cafes = @cafe.filter_by_neighborhood
     render :index
-  end
-
-  private
-
-  def find_cafe
-    @cafe = Cafe.find_by(id: params[:cafe_id])
   end
 
 end
