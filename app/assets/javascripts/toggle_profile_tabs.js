d90bd83595c26fdb6bbcec70fa879df5de460475
Kelly Ripple <email": null>
2015-09-21 10:26:56 -0400

    WIP

---
+++ app/assets/javascripts/toggle_profile_tabs.js
@@ -0,0 +1,27 @@
+function initProfileToggle() {
+
+  // $(".cafes-container").on('click','.tag-container', function(e) {
+  //   e.preventDefault();
+  //   var url = $(this).attr('href');
+  //   var tag = $(this).html();
+  //   $.ajax({url: url, method: "POST", data: {tag: tag}}).done(function(data) {
+  //     $(".cafes-container").html(data)
+  //   }).fail(function(){console.log("failure to send data");});
+  // });
+
+  // $(".tags-topbar").on('click','.tag-container', function(e) {
+  //   e.preventDefault();
+  //   url = $(this).attr('href');
+  //   var tag = $(this).html();
+  //   if(tag == "All") {
+  //     $.ajax(url).done(function(data) {
+  //       $(".cafes-container").html(data)
+  //     }).fail(function(){console.log("failure to retrieve data");});
+  //   } else {
+  //     $.ajax({url: url, method: "POST", data: {tag: tag}}).done(function(data) {
+  //       $(".cafes-container").html(data)
+  //     }).fail(function(){console.log("failure to send data");});
+  //   };
+  // });
+
+};
