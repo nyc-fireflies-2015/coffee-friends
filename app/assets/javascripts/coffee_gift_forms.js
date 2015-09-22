a5a157bc76dbd03de3eedb3dfa15b0726463777a
Kelly Ripple <email": null>
2015-09-22 18:32:34 -0400

    form works

--- app/assets/javascripts/coffee_gift_forms.js
+++ app/assets/javascripts/coffee_gift_forms.js
@@ -1,9 +1,12 @@
 function initGiftFormDropdown() {
   $(".gift-form-dropdown").on('click', function(e) {
-
-    var index = $(this).attr('href').slice(6,7);
-    $.ajax('/cafes/' + index + '/coffee_gifts/new').done(function(data) {
-      $(".gift-form").html(data);
-    });
+    var classStr = $(this).attr('class');
+    var index = classStr.lastIndexOf("menu-item-id-") + 13;
+    var itemId = classStr.slice(index,(index+1));
+    var cafeId = $(this).attr('href').slice(6,7);
+    $.ajax('/cafes/' + cafeId + '/coffee_gifts/new').done(function(data) {
+      var modData = data.replace("$menu_item_id", itemId);
+      $(".gift-form").html(modData);
+    }).fail(function(){console.log("failure to retrieve data");});
   });
 };
