9b98a8700a2a05f460228dd92a777fe3bba2ed59
Jerry Chai <jchai002@gmail.com>
2015-09-21 23:25:59 -0400

    wip

--- Gemfile
+++ Gemfile
@@ -49,9 +49,6 @@ gem 'cloudinary'
 
 gem 'factory_girl_rails'
 
-<<<<<<< HEAD
-gem 'yelp'
-
 group :development do
 	# Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 	gem 'spring'
--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -1,47 +1,27 @@
-function initMap() {
-  var geocoder;
+var geocoder;
   var map;
-  var service;
+  function initMap() {
+    var address = document.getElementById("address").value;
+    geocoder = new google.maps.Geocoder();
 
-
-  var address = document.getElementById("address").value;
-  geocoder = new google.maps.Geocoder();
-
-  var mapOptions = {
-    zoom: 16
-  }
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
+    var mapOptions = {
+      zoom: 16
     }
-  });
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+      geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
 
-    var request = {
-    location: new google.maps.LatLng(-33.8665433,151.1956316),
-    radius: '500',
-    types: ['store']
-  };
-
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-};
-
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      console.log(place)
-      createMarker(results[i]);
-    }
   }
-}
+
+
+
