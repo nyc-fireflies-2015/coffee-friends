ea1136dfe21d1a69f5d69c61bb8222c93ea51d56
Kelly Ripple <email": null>
2015-09-22 13:52:28 -0400

    move price format to a helper

---
+++ app/helpers/format_helper.rb
@@ -0,0 +1,13 @@
+module FormatHelper
+  def format_price(price)
+    if price.to_s.split(".")[1].length == 1
+      return price.to_s << "0"
+    else
+      return price.to_s
+    end
+  end
+
+  def format_phone(num)
+    "(#{num.slice(0,3)}) #{num.slice(3,3)}-#{.slice(6,4)}"
+  end
+end
