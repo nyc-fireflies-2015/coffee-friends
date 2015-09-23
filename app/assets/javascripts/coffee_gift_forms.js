d2c221eb772e04e018dbf20111bbb855211f96a7
Kelly Ripple <email": null>
2015-09-23 16:07:35 -0400

    fix bug by clearing form on toggle

--- app/assets/javascripts/coffee_gift_forms.js
+++ app/assets/javascripts/coffee_gift_forms.js
@@ -4,10 +4,15 @@ function initGiftFormDropdown() {
     var index = classStr.lastIndexOf("menu-item-id-") + 13;
     var itemId = classStr.slice(index,(index+1));
     var cafeId = $(this).attr('href').slice(6,7);
-    $.ajax('/cafes/' + cafeId + '/coffee_gifts/new').done(function(data) {
-      var modData = data.replace("$menu_item_id", itemId);
-      $(".gift-form").html(modData);
-      initDropdowns();
-    }).fail(function(){console.log("failure to retrieve data");});
+    var formContainer = $(this).siblings('.gift-form');
+    if(formContainer.children().length > 0){
+      formContainer.empty();
+    } else {
+      $.ajax('/cafes/' + cafeId + '/coffee_gifts/new').done(function(data) {
+        var modData = data.replace("$menu_item_id", itemId);
+        formContainer.html(modData);
+        initDropdowns();
+      }).fail(function(){console.log("failure to retrieve data");});
+    };
   });
 };
