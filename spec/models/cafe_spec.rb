cf742a3aaaf2217305ff89b31f4c8648b03753f4
leahgoldberg <leahgoldberg31@gmail.com>
2015-09-23 21:47:00 -0400

    add tests for to_param and redeeemed and charitable gifts

--- spec/models/cafe_spec.rb
+++ spec/models/cafe_spec.rb
@@ -9,13 +9,10 @@ RSpec.describe Cafe, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:email) }
-
     it { should validate_length_of(:email).is_at_most(50) }
     it { should validate_length_of(:name).is_at_most(50) }
     it { should validate_length_of(:address).is_at_most(150) }
-
     it { should validate_length_of(:password).is_at_least(6).on(:create) }
-
     it { expect(FactoryGirl.build(:cafe)).to validate_uniqueness_of(:email) }
   end
 
@@ -42,17 +39,24 @@ RSpec.describe Cafe, type: :model do
     end  
   end 
 
-  describe '#unredeemed_coffee_gifts' do 
-  end 
 
   describe '#redeemed_coffee_gifts' do 
-  end 
+    it 'should return a list of all redeemed coffee gifts' do 
+      cafe = FactoryGirl.create(:cafe)
+      menu_item = cafe.menu_items.create(FactoryGirl.attributes_for(:menu_item))
+      redeemed_gifts = (1..5).to_a.map {menu_item.coffee_gifts.create(menu_item: menu_item, charitable: true, redeemed: true)}
+      expect(cafe.redeemed_coffee_gifts.to_a).to eq(redeemed_gifts)
+    end
+  end
 
-  describe '#filter_by_borough' do 
-  end 
+  describe '#to_param' do 
+    it 'should generate the correct slug' do 
+      cafe = FactoryGirl.create(:cafe)
+      cafe.update_attributes(name: "Example Cafe")
+      expect(cafe.slug).to eq("example-cafe")
+    end  
+  end
 
-  describe '#filter_by_neighborhood' do 
-  end 
 end
 
 
