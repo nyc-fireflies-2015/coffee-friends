9512fdc7033ae411cdc01a66de82527df85f53d8
Kelly Ripple <email": null>
2015-09-24 18:12:19 -0400

    add fix for non-single digit ids

--- app/assets/javascripts/coffee_gift_forms.js
+++ app/assets/javascripts/coffee_gift_forms.js
@@ -4,6 +4,7 @@ function initGiftFormDropdown() {
     var itemId = idFinder(classStr, "menu-item-id-");
     var cafeId = idFinder(classStr, "cafe-id-");
     var formContainer = $(this).siblings('.gift-form');
+    debugger
     $("#new_coffee").remove();
     $.ajax('/cafes/' + cafeId + '/coffee_gifts/new').done(function(data) {
       var modData = data.replace("$menu_item_id", itemId);
@@ -14,9 +15,19 @@ function initGiftFormDropdown() {
 
   function idFinder(classStr, finderStr) {
     var index = classStr.indexOf(finderStr) + finderStr.length;
-    return classStr.slice(index,(index+1));
+    var id = "";
+    var i;
+    for( i = 0; 1 == 1 ; i++ ){
+      var nextChar = classStr.slice(index+i,(index+1+i));
+      if (( nextChar === " " )||(nextChar === "")){
+        return id;
+      } else {
+        id = id + nextChar;
+      };
+    };
   };
 };
+
 
 
 
